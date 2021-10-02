bbd3b0f2e3de0939c83488d61c0278a5f4afe99a
thanhvu3105 <60678586+thanhvu3105@users.noreply.github.com>
2021-10-01 19:09:36 -0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -1,10 +1,21 @@
 import requests
 import json
 
-import random
+
+
 
 def get_meme():
-  response = requests.get("http://alpha-meme-maker.herokuapp.com/1")
+  response = requests.get("https://meme-api.herokuapp.com/gimme")
   json_data = json.loads(response.content)
-  meme = json_data["data"][random.randint(1,20)]["image"]
-  return meme
+  return json_data["url"]
+  #print(json_data)
+  #meme = json_data["data"][random.randint(1,20)]["image"]
+  #return meme
+
+def generate_meme():
+  response = requests.get("https://api.memegen.link/images/buzz/memes/memes_everywhere.png")
+  return response.text
+  
+
+
+
